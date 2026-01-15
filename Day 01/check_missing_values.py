0c48156a9f764c0bf2baf69fe964eafe987317d5
Harshita Apankar <harshitaapankar@gmail.com>
2026-01-15 19:23:27 +0530

    Day 1: Create check missing values using isnull() and notnull()

---
+++ Day 01/check_missing_values.py
@@ -0,0 +1,13 @@
+#checking for missing values using isnull() and notnull()
+
+import pandas as pd
+import numpy as np
+
+dict={'first score':[100,90,np.nan,95],
+      'second score':[30,45,56,np.nan],
+      'third score':[np.nan,40,80,98]}
+
+df=pd.DataFrame(dict)
+print(df.isnull())
+
+print(df.notnull())
